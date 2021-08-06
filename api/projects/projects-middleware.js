54ebfee7b7c8b34a757d51f7c31a5e20bcaea61a
Taylor Kang-Illescas <66800091+taylorkangillescas@users.noreply.github.com>
2021-08-06 15:48:16 -0700

    added projects mw

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,36 @@
 // add middlewares here related to projects
+const Project = require('./projects-model')
+
+async function validateProjectId( req, res, next) {
+    try{
+        const project = await Project.get(req.params.id)
+        if(!project) {
+            res.status(404).json({
+                message: "This project couldn't be found"
+            })
+        } else {
+            req.project = project
+            next() 
+        }
+    } catch(err){
+    next(err)
+    }
+}
+
+function validateProject (req, res, next) {
+    const { name, description } = req.body
+    if(!name || !description ){
+        res.status(400).json({
+            message: "Please enter a valid name or description"
+        })
+    } else {
+        next()
+    }
+}
+
+
+
+module.exports = {
+    validateProject,
+    validateProjectId
+}
