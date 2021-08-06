4b47ee9e3df71873f0c724948da76c0f899d0578
Taylor Kang-Illescas <66800091+taylorkangillescas@users.noreply.github.com>
2021-08-06 15:49:26 -0700

    added server js

--- api/server.js
+++ api/server.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const server = express();
+const projectsRouter = require('./projects/projects-router')
+const actionsRouter = require('./actions/actions-router')
+
+server.use(express.json())
+
+server.use('/api/projects', projectsRouter)
+server.use('/api/actions', actionsRouter)
+
+server.get('/', (req,res) => {
+    res.send('<h1> test <h1>')
+})
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
